98f36fadf1ed9db72337694af6335b59ddb6b94a
Dagogo Clinton Uranta <dagogouranta@gmail.com>
2022-09-20 17:10:08 +0100

    indentation on bullet points for login and register screens

--- src/app/main/apps/urbanhive-login/Login.js
+++ src/app/main/apps/urbanhive-login/Login.js
@@ -163,10 +163,9 @@ function Login() {
               <Typography variant="subtitle1" color="inherit" className="mt-32">
                 Your powerful and professional Co-Founder Matching Platform 😊.
                
-              <br/> <br/>
               <ul style={{listStyle: "square", color:"#FFFFF"}}>
-                 <li>Coding Sessions</li>
-                 <li>Book a Developer</li>
+                &nbsp; &nbsp; <li>Coding Sessions</li>
+                &nbsp; &nbsp; <li>Book a Developer</li>
                </ul>
               </Typography>
             </motion.div>
